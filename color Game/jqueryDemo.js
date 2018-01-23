6d84cc31be6ed90eb8a356865448412a52a142ac
nullidentifier <tasleemhalai@gmail.com>
2018-01-22 23:15:14 -0500

    Jquery demo for selecting and changing properties of DOM objects

--- color Game/jqueryDemo.js
+++
@@ -1,7 +0,0 @@
-$("div").css("background","purple");
-$(".highlight").css("width","200px");
-
-$("#third").css("border","1px orange solid");
-
-$("div:first-of-type").css("color","pink");
-//or $("div:first").css("color","pink"); //this is slower as it is jquery shortcut where else the above one is inbuild func of css
